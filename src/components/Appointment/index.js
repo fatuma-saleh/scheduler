7650024d897b535b3c371d27ec2370f2af459fa2
Fatuma <sfatex@gmail.com>
2021-11-11 14:04:48 -0500

    Rendered the appointmnet component conditionaly

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,7 +2,7 @@
  import 'components/Appointment/styles.scss'
 
 export default function Appointment(props) {
-  console.log(";;",props)
+   
    return (
  <article className="appointment">{props.time ? `Appointment at ${props.time}`: `No Appointments`} </article> 
   
