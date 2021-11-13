c47d982a100771fa672e15809773cfaacba6653b
Fatuma <sfatex@gmail.com>
2021-11-12 19:42:18 -0500

    Update the Appointment component to render the Show and Empty views conditionaly

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,18 @@
- import React from "react";
+ import React from "react"
  import 'components/Appointment/styles.scss'
+ import Header from "./Header"
+ import Empty from "./Empty"
+ import Show from "./Show"
+
 
 export default function Appointment(props) {
-   
+   console.log("lastttt",props)
    return (
- <article className="appointment">{props.time ? `Appointment at ${props.time}`: `No Appointments`} </article> 
-  
+     <>
+  <Header time={props.time}/>   
+ {/* <article className="appointment">{props.time ? `Appointment at ${props.time}`: `No Appointments`} </article>  */}
+ <article className="appointment">{props.interview ? <Show student= {props.interview.student} interviewer={props.interview.interviewer}/>: <Empty/>} </article> 
+  </>
    );
 
 
