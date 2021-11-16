cab4782d610e9970cadbabb41d6fdb6ecde36fa6
Fatuma <sfatex@gmail.com>
2021-11-16 12:33:37 -0500

    Remove hard coded Days array and fetch days from api using axios and useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
+import axios from "axios"
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 //Appointments
 
 const appointments = {
@@ -68,16 +52,26 @@ const appointments = {
 
 
 export default function Application(props) {
+
   const [day, setDay] = useState("Monday");
-  
-  const appointmentArray = Object.values(appointments).map((appointment)=>{
+  const [days, setDays] = useState([]);
+  const appointmentArray = Object.values(appointments).map((appointment) => {
+
     return (
-    <Appointment 
-    key={appointment.id} 
-    {...appointment} 
-  />
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
     )
-} );
+  });
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/api/days`).then(response => {
+
+      console.log("response days", response);
+      setDays(response.data)
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -103,7 +97,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointmentArray}
-      
+
       </section>
     </main>
   );
