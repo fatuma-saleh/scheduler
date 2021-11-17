9a72e82a23aa55813e48a3856127934581f2f834
Fatuma <sfatex@gmail.com>
2021-11-17 12:48:43 -0500

    Add selector function to get interviewers along with test cases

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,4 @@
 
-//... returns an array of appointments for that day
 export function getAppointmentsForDay(state, dayName) {
 
   for (const day of state.days) {
@@ -14,4 +13,13 @@ export function getAppointmentsForDay(state, dayName) {
 
   }
   return []
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewObj = { student: interview.student, interviewer: state.interviewers[interview.interviewer] }
+  return interviewObj
+
 }
