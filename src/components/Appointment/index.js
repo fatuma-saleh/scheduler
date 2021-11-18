229ae40c9d9101b35ef428f8dd4bc243b1189c35
Fatuma <sfatex@gmail.com>
2021-11-18 12:11:00 -0500

    Use custom hooks to render different views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,19 +1,33 @@
- import React from "react"
- import 'components/Appointment/styles.scss'
- import Header from "./Header"
- import Empty from "./Empty"
- import Show from "./Show"
+import React from "react"
+import 'components/Appointment/styles.scss'
+import Header from "./Header"
+import Empty from "./Empty"
+import Show from "./Show"
+import Form from "./Form"
+import useVisualMode from "hooks/useVisualMode"
 
-
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE ="CREATE"
 export default function Appointment(props) {
-   console.log("lastttt",props)
+   const { mode, transition, back } = useVisualMode(
+      props.interview ? SHOW : EMPTY
+   );
    return (
-     <>
-  <Header time={props.time}/>   
- {/* <article className="appointment">{props.time ? `Appointment at ${props.time}`: `No Appointments`} </article>  */}
- <article className="appointment">{props.interview ? <Show student= {props.interview.student} interviewer={props.interview.interviewer}/>: <Empty/>} </article> 
-  </>
+      <>
+         <Header time={props.time} />
+         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+         {mode === SHOW && (
+            <Show
+               student={props.interview.student}
+               interviewer={props.interview.interviewer}
+            />
+         )}
+         {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
+         {/* <article className="appointment">{props.interview ? <Show student= {props.interview.student} interviewer={props.interview.interviewer}/>: <Empty/>} </article>  */}
+      </>
    );
 
 
 }
+
