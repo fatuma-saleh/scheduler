669cf7b13a896ec157486b07dfc5eb10e765c78d
Fatuma <sfatex@gmail.com>
2021-11-22 20:23:31 -0500

    Refactor the custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,35 +5,35 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
 
-  //const [mode, setMode] = useState(initial);
+  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial])
 
-  function transition(mode, replace = false) {
-     setHistory((pre)=> replace?[...pre.slice(0,-1),mode]:[...pre,mode])
-    // setMode(mode);
-    // if (!replace) {
-    //   history.push(mode)
-    //   setHistory(history)
-    //   //setMode(history)
-    // }
-    // else {
-    //   history.slice(0, -1)
-    //   //history.push(newMode)
-    //   console.log("skipped history",history)
-    //   setHistory(history)
-    // }
+  function transition(newMode, replace = false) {
+   setMode(newMode)
+  if(replace){
+    const newHistory=[...history]
+    newHistory[newHistory.length-1] = newMode
+    setHistory(newHistory)
+  }else{
+  const newHistory=[...history,newMode]
+  setHistory(newHistory)
+   // setHistory((pre) => replace ? [...pre.slice(0, -1), mode] : [...pre, mode])
+  }
+   
   }
   function back() {
-     //if (history.length > 1) {
-    //   history.pop()
-    //   setHistory(history)
-      //setMode(history[history.length - 1])
-    // }
-setHistory((pre)=>pre.length>1?pre.slice(0,-1): pre)
+  if(history.length > 1){
+  const newHistory = [...history]
+  newHistory.pop()
+  const lastMode = newHistory[newHistory.length-1]
+  setMode(lastMode)
+  setHistory(newHistory)
+  }
+  //setHistory((pre) => pre.length > 1 ? pre.slice(0, -1) : pre)
 
   }
-  //return { mode, transition, back };
-  return { mode:history[history.length-1], transition, back };
+  return { mode, transition, back };
+  //return { mode: history[history.length - 1], transition, back };
 
 
 }
