c1ee4ae9dcaa3676cd71e1d96f7893dfd8585e81
Fatuma <sfatex@gmail.com>
2021-12-02 12:05:47 -0500

    mock axios.put

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,6 +54,9 @@ const fixtures = {
 };
 
 export default {
+  put: jest.fn(() =>{ 
+    return Promise.resolve({ status: 204, statusText: "No Content" });
+  }), 
   defaults: { baseURL: "" },
   get: jest.fn((url) => {
     if (url === "/api/days") {
